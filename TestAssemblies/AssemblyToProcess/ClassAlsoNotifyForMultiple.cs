0784f4f67b0c677d5fa45245e06d3b3528f8723a
Simon Cropp <simon.cropp@gmail.com>
2013-03-14 17:53:42 +1100

    more tests

---
+++ TestAssemblies/AssemblyToProcess/ClassAlsoNotifyForMultiple.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using PropertyChanging;
+
+public class ClassAlsoNotifyForMultiple : INotifyPropertyChanging
+{
+    [AlsoNotifyFor("Property2", "Property3")]
+    public string Property1 { get; set; }
+    public string Property2 { get; set; }
+    public string Property3 { get; set; }
+    
+    public event PropertyChangingEventHandler PropertyChanging;
+}
