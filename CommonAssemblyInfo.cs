c82ccf6fbb5a0e2052a7e37c90d422c463c1a40d
Simon Cropp <simon.cropp@gmail.com>
2013-04-01 22:55:41 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanging")]
 [assembly: AssemblyProduct("PropertyChanging")]
-[assembly: AssemblyVersion("1.21.1.0")] 
-[assembly: AssemblyFileVersion("1.21.1.0")]
+[assembly: AssemblyVersion("1.21.1.1")] 
+[assembly: AssemblyFileVersion("1.21.1.1")]
  
