8047af89ba2066e376ced98870ceca348eab342f
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 20:18:14 +1000

    support for TFS checkout

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanging")]
 [assembly: AssemblyProduct("PropertyChanging")]
-[assembly: AssemblyVersion("1.25.1.0")] 
-[assembly: AssemblyFileVersion("1.25.1.0")]
+[assembly: AssemblyVersion("1.25.2")] 
+[assembly: AssemblyFileVersion("1.25.2")]
  
