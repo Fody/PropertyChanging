266c845f6161a8ff112cb53b14b6fc0c5f5a8e6c
Simon Cropp <simon.cropp@gmail.com>
2014-05-16 09:47:04 +1000

    SN

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanging")]
 [assembly: AssemblyProduct("PropertyChanging")]
-[assembly: AssemblyVersion("1.25.5")] 
-[assembly: AssemblyFileVersion("1.25.5")]
+[assembly: AssemblyVersion("1.25.6")] 
+[assembly: AssemblyFileVersion("1.25.6")]
  
