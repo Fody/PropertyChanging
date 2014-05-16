ae28f72d9d3472986f8513b390e0fd11b455640e
Simon Cropp <simon.cropp@gmail.com>
2014-05-16 10:08:03 +1000

    fix xml comment error

--- PropertyChanging/ImplementPropertyChangingAttribute.cs
+++ PropertyChanging/ImplementPropertyChangingAttribute.cs
@@ -4,7 +4,7 @@ namespace PropertyChanging
 {
     /// <summary>
     /// Include a <see cref="Type"/> for notification.
-    /// The <see cref="System.ComponentModel.INotifyPropertyChanging"/> interface is added to the type.
+    /// The INotifyPropertyChanging interface is added to the type.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class ImplementPropertyChangingAttribute : Attribute
