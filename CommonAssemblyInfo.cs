9bbd54c6be05159e12b840644ace973aeadf47d0
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 14:01:55 +1000

    better universal support

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanging")]
 [assembly: AssemblyProduct("PropertyChanging")]
-[assembly: AssemblyVersion("1.27.0")] 
-[assembly: AssemblyFileVersion("1.27.0")]
+[assembly: AssemblyVersion("1.27.1")] 
+[assembly: AssemblyFileVersion("1.27.1")]
  
