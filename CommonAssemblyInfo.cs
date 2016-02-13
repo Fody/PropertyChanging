24ba8ff8d68f181eac5fcab22ab729343e5a8dd8
Simon Cropp <simon.cropp@gmail.com>
2016-02-13 13:58:04 +1100

    remove msbuild hack

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanging")]
 [assembly: AssemblyProduct("PropertyChanging")]
-[assembly: AssemblyVersion("1.27.1")] 
-[assembly: AssemblyFileVersion("1.27.1")]
+[assembly: AssemblyVersion("1.27.2")] 
+[assembly: AssemblyFileVersion("1.27.2")]
  
